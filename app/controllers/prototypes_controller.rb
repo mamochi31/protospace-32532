6a84694705e5ef29dad3824e0715a7e9bf4fef59
mamochi31 <mamoruchikara9631@gmail.com>
2020-12-04 15:27:36 +0900

    アクセス制限の修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,6 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :move_to_index, only: [:edit]
+  before_action :move_to_index, only: :edit
 
   def index
     @prototypes = Prototype.all
@@ -54,9 +54,8 @@ class PrototypesController < ApplicationController
   end
 
   def move_to_index
-    unless current_user.id == params[:id]
-      redirect_to action: :index
-    end
+    @prototype = Prototype.find(params[:id])
+    redirect_to action: :index if current_user.id != @prototype.user_id
   end
 
 end
